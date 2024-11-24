793628cf6d20bd19716839a397329937f79dd62b
Jayash Saini <jayashysaini7361@gmail.com>
2024-11-25 01:43:41 +0530

    config(vercel): add vercel config and improve UI

--- Frontend/src/pages/home.tsx
+++ Frontend/src/pages/home.tsx
@@ -39,7 +39,7 @@ export default function home() {
         The road to freedom starts from here
       </p>
       <TypewriterEffect words={words} />
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
+      <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 space-x-0 md:space-x-4 mt-10">
         <button
           onClick={handleCreatePoll}
           className="w-40 h-10 rounded-xl bg-slate-900 border  border-transparent text-white text-sm"
